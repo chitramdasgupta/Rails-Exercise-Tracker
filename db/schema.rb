94b4e3964d36aecc26ebf16c7deaa6e2c81380de
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-01 13:31:53 +0530

    Created Exercises controller and Exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_065431) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_080128) do
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
