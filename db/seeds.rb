b6ab0bd375261d644b06e8e9e9ffdad0c299a62d
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-01 12:39:17 +0530

    Seeded three sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'abc@gmail.com', password: 'abc', password_confirmation: 'abc')
+
+User.create(email: 'xyz@gmail.com', password: 'xyz', password_confirmation: 'xyz')
+
+User.create(email: '123@gmail.com', password: '123', password_confirmation: '123')
