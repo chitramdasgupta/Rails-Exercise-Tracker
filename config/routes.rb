7e00bf65533ddf27901be151b57cb1afffabfd18
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-01 13:27:48 +0530

    Implemented the login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'login', to: 'sessions#new'
+  #post 'login', to: 'sessions#create'
+  #delete 'logout', to: 'sessions#destroy'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
