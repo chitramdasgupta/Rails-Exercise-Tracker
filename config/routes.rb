431d256e12266dcb29dc64559d7aa143d687e75e
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-01 12:22:04 +0530

    Basic navbar setup done and about page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/index'
+  get 'homes/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
