475f4efc456a218fa5e28b5a3e7a78efee9c2499
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-02 12:43:10 +0530

    Exercise creation and display functionality added

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,14 +1,25 @@
 class ExercisesController < ApplicationController
   def index
+    @exercises = Exercise.all
   end
 
   def new
+    @exercise = Exercise.new
   end
 
   def create
+    @exercise = Exercise.new(exercise_params)
+
+    if @exercise.save
+      flash[:success] = "Exercise added"
+      redirect_to exercises_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @exercise = Exercise.find(params[:id])
   end
 
   def edit
@@ -19,4 +30,11 @@ class ExercisesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :calories)
+  end
+
 end
