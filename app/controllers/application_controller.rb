8005212321ef46f271b0b1b94fe5545d34af7379
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-02 20:45:53 +0530

    Extracted logic code from view into a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :require_authorization
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
     User.find_by(id: session[:user_id])
