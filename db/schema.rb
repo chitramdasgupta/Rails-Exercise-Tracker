b777411fb59e19e895fc6835bcc23617abd0b27d
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-03 12:59:33 +0530

    Generated migrations to add username, phone number to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_065233) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_072125) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.integer "calories"
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_065233) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "phone_number"
   end
 
   add_foreign_key "exercises", "users"
