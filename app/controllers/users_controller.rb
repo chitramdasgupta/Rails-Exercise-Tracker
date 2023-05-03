d823d3166143c32dd78cbf860288ed2decf7ebb9
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-05-03 16:52:45 +0530

    Added the functionality to add profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :username, :phone_number)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :phone_number, :profile_picture)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :exercises
 
+  has_one_attached :profile_picture
+
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
